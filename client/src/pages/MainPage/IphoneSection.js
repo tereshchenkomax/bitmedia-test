c21bfb70416a3b31516bc2333359aad751ced847
tereshchenkomax <tereshchenkomax@gmail.com>
2019-12-18 10:13:39 +0200

    styling

--- client/src/pages/MainPage/IphoneSection.js
+++ client/src/pages/MainPage/IphoneSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from "../../components/Button";
+import iPhone from '../../iPhone.png'
 
 const IphoneSection = () => {
 	return (
 		<section className='iphone-section'>
 			<div className="row">
-				<div className='col-1-of-2'>
+				<div className='col-2-of-3'>
 					<div className="has-margin-top-medium has-margin-bottom-medium">
 						<h1>
 							<strong>Brainstorming</strong> for desired perfect Usability
@@ -19,9 +20,9 @@ const IphoneSection = () => {
 					</p>
 					<Button text='View Stats'/>
 				</div>
-				<div className='col-1-of-2'>
+				<div className='col-1-of-3'>
 					<div>
-
+						<img src={iPhone} alt=""/>
 					</div>
 				</div>
 			</div>
