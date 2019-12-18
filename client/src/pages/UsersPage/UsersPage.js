9e5b453dd6cc00d4a4368005d31ce2dadf2a5264
tereshchenkomax <tereshchenkomax@gmail.com>
2019-12-18 22:14:24 +0200

    /users endpoint query example http://localhost:3001/users?current_page=2&users_number=50

--- client/src/pages/UsersPage/UsersPage.js
+++ client/src/pages/UsersPage/UsersPage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import Header from "../../Header";
-import {Switch} from "react-router-dom";
 
 const UsersPage = () => {
 	return (
--- routes/users.js
+++ routes/users.js
@@ -2,30 +2,25 @@ const express = require('express');
 const router = express.Router();
 const users = require('../users');
 const users_statistic = require('../users_statistic');
-const getJsonFromUrl = require('../helpers/getJsonFromUrl');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-	const urlParams = new URLSearchParams(req.url);
-	const myParam = urlParams.get('current_page');
-	const myParam1 = urlParams.get('users_number');
-	let jsonaaa = getJsonFromUrl(req.url);
-	const result = users.map(a => {
-		let userclicks = 0;
-		let userviews = 0;
-		users_statistic.filter(b => {
-			if (b.user_id === a.id) {
-				userclicks += b.clicks;
-				userviews += b.page_views
-			}
-		});
+    const users_number = req.query.users_number || 50;
+    const current_page = req.query.current_page || 1;
+	const usersSlised = users.slice(users_number * current_page - users_number, users_number * current_page);
+    const result = usersSlised.filter(a => {
+        let totalClicks = 0;
+        let totalViews = 0;
+        users_statistic.filter(b => {
+            if (b.user_id === a.id) {
+                totalClicks += b.clicks;
+                totalViews += b.page_views
+            }
+        });
 
-		return {...a, userclicks, userviews}
-	});
-	console.log(jsonaaa);
-	console.log(myParam,myParam1);
-
-	res.send(result);
+        return {...a, totalClicks, totalViews}
+    });
+    res.send(result);
 });
 
 module.exports = router;
